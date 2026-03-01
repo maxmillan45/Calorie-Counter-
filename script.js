0de6c329e946ad6d425c5efd1d230c18e881728f
maxmillan45 <maxmillannjue@gmail.com>
2026-03-01 15:37:15 +0300

    add style.css

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get elements
+var form = document.getElementById("food-form");
+var foodNameInput = document.getElementById("food-name");
+var calorieInput = document.getElementById("calories");
+var foodList = document.getElementById("food-list");
+var totalCaloriesDisplay = document.getElementById("total-calories");
+var resetBtn = document.getElementById("reset-btn");
