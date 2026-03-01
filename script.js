958b823979dd8307cce6ec7f434219b6bcd17d7c
maxmillan45 <maxmillannjue@gmail.com>
2026-03-01 15:39:42 +0300

    add script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ var calorieInput = document.getElementById("calories");
 var foodList = document.getElementById("food-list");
 var totalCaloriesDisplay = document.getElementById("total-calories");
 var resetBtn = document.getElementById("reset-btn");
+
+// Load saved data
+var foods = JSON.parse(localStorage.getItem("foods")) || [];
+
+// Save to localStorage
+function saveData() {
+  localStorage.setItem("foods", JSON.stringify(foods));
+}
